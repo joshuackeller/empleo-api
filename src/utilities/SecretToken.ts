045741725569c42320ca8a3252030fc321b1da9f
joshuackeller <joshuackeller@gmail.com>
2023-12-23 12:08:36 -0700

    fixes

--- src/utilities/SecretToken.ts
+++
@@ -1,7 +0,0 @@
-const SecretToken = {
-  auth: process.env.JWT_SECRET,
-  confirm_account: process.env.JWT_SECRET + "confirm_account",
-  reset_password: process.env.JWT_SECRET + "reset_password",
-};
-
-export default SecretToken;
--- src/utilities/nano_id.ts
+++
@@ -1,7 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-const alphabet =
-  "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-const nano_id = customAlphabet(alphabet, 12);
-
-export default nano_id;
