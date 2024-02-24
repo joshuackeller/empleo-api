e6fa9aa7db660c9cb515f7016b0e6c0ad05d2f63
emilyjensen12 <emily12jensen@gmail.com>
2024-02-24 00:04:04 -0700

    wip

--- api/client/listings/index.ts
+++ api/client/listings/index.ts
@@ -1,7 +1,7 @@
 import prisma from "../../../src/utilities/prisma";
 import express from "express";
 import handler from "../../../src/middleware/handler";
-import { EmpleoRequest } from "../../../src/utilities/interfaces";
+import { ClientRequest } from "../../../src/utilities/interfaces";
 import {
   ClientApplicationSelect,
   ClientListingSelect,
@@ -12,13 +12,11 @@ import { ListingSelect } from "../../../src/select/admin";
 import { z } from "zod";
 import nano_id from "../../../src/utilities/nano_id";
 
-//make new client listings select
-
 const router = express.Router();
 
 router.get(
   "/",
-  handler(async (req: EmpleoRequest, res) => {
+  handler(async (req: ClientRequest, res) => {
     console.log(req.headers.organization);
     const Listings = await prisma.listing.findMany({
       where: {
@@ -33,7 +31,7 @@ router.get(
 
 router.get(
   "/:listingId",
-  handler(async (req: EmpleoRequest, res) => {
+  handler(async (req: ClientRequest, res) => {
     const { listingId } = z
       .object({
         listingId: z.string(),
@@ -51,44 +49,5 @@ router.get(
     res.json(listing);
   })
 );
-
-// router.post(
-//   "/:listingId",
-//   handler(async (req: EmpleoRequest, res) => {
-//     const body = z
-//       .object({
-//         firstName: z.string(),
-//         lastName: z.string(),
-//         phone: z.string().optional(),
-//         //gender: z.string().optional(),
-//         address: z.string().optional(),
-//         city: z.string().optional(),
-//         state: z.string().optional(),
-//         zip: z.string().optional(),
-//         usCitizen: z.boolean().optional(),
-//         workVisa: z.boolean().optional(),
-//         workVisaType: z.string().optional(),
-//         language: z.string().optional(),
-//         availableStartDate: z.string().optional(),
-//         note: z.string().optional(),
-//         relocate: z.boolean().optional(),
-//         userId: z.string(),
-//       })
-//       .parse(req.body);
-
-//     const application = await prisma.application.create({
-//       data: {
-//         id: nano_id(),
-//         user: { connect: {} },
-
-//         organization: { connect: { id: req.organizationId } },
-//         ...body,
-//       },
-//       select: ClientApplicationSelect,
-//     });
-
-//     res.json(application);
-//   })
-// );
 
 export default router;
